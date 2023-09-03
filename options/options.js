7b4d3edfe73cfa1ad38e3c03b6620fbe65b9b200
Crowdbotics <team@crowdbotics.com>
2023-09-03 18:08:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "datafund-43216",
+  url: "https://datafund-43216.botics.co",
+  api: "https://datafund-43216.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
